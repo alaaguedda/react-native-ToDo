6d149fea07cf3351b47dad621080aa9d1c9771a3
alaaguedda <alaarodri10@gmail.com>
2024-04-26 17:45:46 +0100

    initial commit for reactnative todo app

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskItems, setTaskItems] = useState([]);
+  const inputRef = useRef(null);
+
+  const handleAddTask = () => {
+    if (task.trim() !== '') {
+      Keyboard.dismiss();
+      setTaskItems([...taskItems, task]);
+      setTask('');
+    }
+  };
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
+  const onFocusInput = () => {
+    inputRef.current.focus();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}> TODAY'S TASKS</Text>
+        <ScrollView style={styles.scrollView}>
+          <View style={styles.items}>
+            {taskItems.map((item, index) => (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : null}
+        style={[styles.writeTaskWrapper, task !== '' && styles.writeTaskWrapperActive]}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={'write a task'}
+          value={task}
+          onChangeText={text => setTask(text)}
+          onFocus={onFocusInput}
+          ref={inputRef}
+        />
+        <TouchableOpacity onPress={handleAddTask}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +65,62 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+    flex: 1,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom:20,
+  },
+  scrollView: {
+    flex: 1,
+  },
+  items: {
+    marginBottom: 100,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingBottom: Platform.OS === 'ios' ? 20 : 0,
+    backgroundColor: '#E8EAED',
+    
+    borderTopColor: '#C0C0C0',
+  },
+  writeTaskWrapperActive: {
+    position: 'relative',
+    bottom: Platform.OS === 'ios' ? null : 0,
+  },
+  input: {
+    flex: 1,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    marginRight: 20,
+    marginBottom:10,
+    marginTop:10,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {}
 });
